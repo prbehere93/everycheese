7372c992ea36de66f897133b53a260c53eb5024e
prbehere93 <prbehere93@gmail.com>
2021-04-05 11:24:03 +0530

    added the list to navbar

--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
     ),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    #urls for the cheese app
+    path('cheeses/', include('everycheese.cheeses.urls', namespace='cheeses')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import cheese
 
-# Create your views here.
+#a simple view which lists all the cheeses on a single page
+class CheeseListView(ListView):
+    model=cheese
+
