b462699bbec555d727d78f3bc91dc605f364b1ca
prbehere93 <prbehere93@gmail.com>
2021-04-09 00:05:36 +0530

    updated test_models

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -9,3 +9,8 @@ def test___str__():
     cheese1 = CheeseFactory()
     assert cheese1.__str__() == cheese1.name
     assert str(cheese1) == cheese1.name
+
+def test_get_absolute_url():
+    cheese1=CheeseFactory()
+    url=cheese1.get_absolute_url()
+    assert url == f'/cheeses/{cheese1.slug}'
