5d4cf7114d5ea0927b45cda8be66160960169d21
prbehere93 <prbehere93@gmail.com>
2021-04-08 23:57:11 +0530

    added creator to CheeseFactory

--- everycheese/cheeses/tests/factories.py
+++ everycheese/cheeses/tests/factories.py
@@ -1,6 +1,7 @@
 from django.template.defaultfilters import slugify
 import factory
 import factory.fuzzy
+from everycheese.users.tests.factories import UserFactory
 
 from ..models import cheese
 
@@ -10,6 +11,7 @@ class CheeseFactory(factory.django.DjangoModelFactory):
     description=factory.Faker('paragraph', nb_sentences=3, variable_nb_sentences=True)
     firmness = factory.fuzzy.FuzzyChoice([x[0] for x in cheese.Firmness.choices])
     country_of_origin = factory.Faker('country_code')
+    creator=factory.SubFactory(UserFactory) #creates a user
 
     class Meta:
         model=cheese
